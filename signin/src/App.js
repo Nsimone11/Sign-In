a45546238fa61a34f6e031361aae1de749e183dc
Nsimone11 <nsimonepettis@gmail.com>
2022-07-12 12:23:38 +0000

    first commit

---
+++ signin/src/App.js
@@ -0,0 +1,12 @@
+//App.js
+import React from "react";
+import './App.css';
+import Login from "./components/Login";
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+}
+export default App;
